11a1986690aa298edf31408ff346b395d556e570
gilmar <gilmar_custodio_@hotmail.com>
2020-04-07 20:47:06 -0300

    systems now work in observer pattern

--- systems/base_system.hpp
+++ systems/base_system.hpp
@@ -1,49 +1,33 @@
 #pragma once
 
 #include <tuple>
-#include <vector>
+#include <set>
 
 #include "../dependencies/mp/types/types_list.hpp"
 #include "../entities/entity.hpp"
 
-    template<typename TSettings, typename TSignature, typename Derived>
+namespace ecs
+{
+    template<typename Derived>
     class BaseSystem
     {
     public:
-        using Settings = TSettings;
-        using ComponentList = typename Settings::ComponentList;
-        using TagList = typename Settings::TagList;
-        using Bitset = typename Settings::Bitset;
-        using Signature = TSignature;
-
         BaseSystem() : m_registered_entities()
         {
-            ComponentList comps{};
-            TagList tags{};
-            mp::for_tuple([this] (auto comp)
-            {
-                using Component = decltype(comp);
-
-                if(mp::contains<Component, Signature>{})
-                    this->m_signature[Settings::template component_bit<Component>()] = true;
-            }, comps);
-
-            mp::for_tuple([this] (auto tag) {
-                using Tag = decltype(tag);
-
-                if(mp::contains<Tag, Signature>{})
-                    this->m_signature[Settings::template tag_bit<Tag>()] = true;
-            }, tags);
         }
 
-        void update(Signature& signature)
+        void update(float delta_time)
         {
-            static_cast<Derived*>(this)->update(signature);
+            static_cast<Derived*>(this)->update(delta_time);
         }
 
-        auto& get_signature() const { return m_signature; }
+        void register_entity(EntityID id)
+        {
+            m_registered_entities.insert(id);
+        }
+        //auto& get_signature() const { return m_signature; }
 
-    private:
-        std::vector<EntityID> m_registered_entities;
-        Bitset m_signature;
+    protected:
+        std::set<EntityID> m_registered_entities;
     };
+}
