c999cea098ca6b498bac305209d2290a840a8016
Gilmar <gilmar_custodio_@hotmail.com>
2020-11-19 15:11:58 -0300

    system now uses sparse_sets to register entities

---
+++ core/sparse_set.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+namespace ecs
+{
+	template<typename T>
+	class sparse_set
+	{
+	public:
+		sparse_set(unsigned capacity = 512u) 
+		{
+			m_dense.reserve(capacity);
+			m_sparse.reserve(capacity);
+		}
+		~sparse_set() = default;
+
+		void insert(T n)
+		{
+			if (contains(n)) return;
+
+			m_sparse[n] = m_dense.size();
+			m_dense.push_back(n);
+
+			m_sorted = false;
+		}
+		void remove(T n)
+		{
+			m_dense[m_sparse[n]] = m_dense[m_dense.size() - 1];
+			m_sparse[m_dense[m_dense.size() - 1]] = m_sparse[n];
+			m_dense.pop_back();
+
+			m_sorted = false;
+		}
+
+		inline bool contains(T n) const
+		{
+			return m_sparse[n] < m_dense.size() && m_dense[m_sparse[n]] == n;
+		}
+
+		inline void clear()
+		{
+			m_dense.clear();
+		}
+		
+		void resize(unsigned size)
+		{
+			m_dense.reserve(size);
+			m_sparse.reserve(size);
+		}
+
+		void sort()
+		{
+			if (m_sorted) return;
+
+			std::sort(m_dense.begin(), m_dense.end());
+			
+			for (T i = 0; i < m_dense.size(); i++)
+			{
+				m_sparse[m_dense[i]] = i;
+			}
+
+			m_sorted = true;
+		}
+
+		auto begin() const {
+			return m_dense.begin();
+		}
+
+		auto end() const {
+			return m_dense.end();
+		}
+
+	private:
+		std::vector<T> m_dense;
+		std::vector<T> m_sparse;
+		bool m_sorted;
+	};
+}
--- systems/base_system.hpp
+++ systems/base_system.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <set>
-
 #include "../dependencies/mp/types/types_list.hpp"
 #include "../core/type_defs.hpp"
+#include "../core/sparse_set.hpp"
 
 namespace ecs
 {
@@ -28,10 +27,10 @@ namespace ecs
 
         void unregister_entity(EntityID id)
         {
-            m_registered_entities.erase(id);
+            m_registered_entities.remove(id);
         }
 
     protected:
-        std::set<EntityID> m_registered_entities;
+        sparse_set<EntityID> m_registered_entities;
     };
 }
