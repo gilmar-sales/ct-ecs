3958be530badf2f0ae101ff597fb35953e96afcb
gilmarxd <gilmar_custodio_@hotmail.com>
2021-06-19 09:29:48 -0300

    🩹  add missing headers

--- include/ecs/ecs.hpp
+++ include/ecs/ecs.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "../../src/core/type_defs.hpp"
 #include "../../src/managers/entity_manager.hpp"
 #include "../../src/managers/component_manager.hpp"
 #include "../../src/managers/manager.hpp"
 #include "../../src/managers/settings.hpp"
+#include "../../src/systems/base_system.hpp"
