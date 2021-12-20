0450437b412b00fb968c81750fef76f70730d11c
gilmarxd <gilmar_custodio_@hotmail.com>
2021-12-20 08:30:33 -0300

    🐛 fix bug of realocation of mesh and set precompile headers

--- examples/space/core/application.cpp
+++ examples/space/core/application.cpp
@@ -6,26 +6,29 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 
-Application* Application::instance = nullptr;
+Application *Application::instance = nullptr;
 
-Application::Application(): window("Space", 800, 600), mgr(ECSManager())
+Application::Application() : window("Space", 800, 600), mgr(ECSManager())
 {
     assert(!instance);
     instance = this;
-    
+
     init_player();
+    // glfwSwapInterval(2);
 }
 
 Application::~Application()
 {
 }
 
-void Application::run() {
+void Application::run()
+{
     float previous_time = glfwGetTime();
     float frametime_accumulator = 0;
     float frames = 0;
-    while(!glfwWindowShouldClose(window.get_native_window())) {
-        
+    while (!glfwWindowShouldClose(window.get_native_window()))
+    {
+
         float now = glfwGetTime();
         Time::delta_time = now - previous_time;
         previous_time = now;
@@ -33,11 +36,13 @@ void Application::run() {
         frametime_accumulator += Time::delta_time;
         frames += 1;
 
-        if (frametime_accumulator >= 1) {
+        if (frametime_accumulator >= 1)
+        {
             std::stringstream ss;
-            ss << window.get_title() << " - " << frames << " FPS";
+            ss << window.get_title() << " - " << frames << " FPS - Entities: ";
+            ss << mgr.get_next_entity();
             glfwSetWindowTitle(window.get_native_window(), ss.str().c_str());
-            
+
             frametime_accumulator = 0;
             frames = 0;
         }
@@ -49,18 +54,19 @@ void Application::run() {
     }
 }
 
-void Application::init_player() {
-    
+void Application::init_player()
+{
+
     auto ent = mgr.create_entity();
 
-    ecs::TransformComponent& transform = mgr.add_component<ecs::TransformComponent>(ent);
-    ecs::MeshComponent& mesh = mgr.add_component<ecs::MeshComponent>(ent);
-    ecs::RigidBodyComponent& rigidbody = mgr.add_component<ecs::RigidBodyComponent>(ent);
+    ecs::TransformComponent &transform = mgr.add_component<ecs::TransformComponent>(ent);
+    ecs::MeshComponent &mesh = mgr.add_component<ecs::MeshComponent>(ent);
+    ecs::RigidBodyComponent &rigidbody = mgr.add_component<ecs::RigidBodyComponent>(ent);
     mgr.add_tag<ecs::PlayerTag>(ent);
 
-    transform.position = {0,0,0};
-    transform.scale = {20,20,20};
-    transform.rotation = {0,0,0};
+    transform.position = {0, 0, 0};
+    transform.scale = {20, 20, 20};
+    transform.rotation = {0, 0, 0};
 
     mesh.VAO = initTriangleMesh();
     rigidbody.mass = 2;
--- examples/space/core/application.hpp
+++ examples/space/core/application.hpp
@@ -1,100 +1,94 @@
 #pragma once
 
-#include <ecs/ecs.hpp>
-
-#include "../systems/physics_system.hpp"
-#include "../systems/render_system.hpp"
-#include "../systems/player_move_system.hpp"
-#include "../systems/meteor_manager_system.hpp"
-
-#include "../components/transform_component.hpp"
-#include "../components/mesh_component.hpp"
-#include "../tags/tags.hpp"
-
 #include "window.hpp"
-
-using ComponentList = std::tuple<ecs::TransformComponent, ecs::MeshComponent, ecs::RigidBodyComponent>;
-using TagList = std::tuple<ecs::PlayerTag>;
-using SystemList = std::tuple<ecs::PhysicsSystem, ecs::RenderSystem, ecs::PlayerMoveSystem, ecs::MeteorManagerSystem>;
-using Settings = ecs::Settings<ComponentList, TagList, SystemList>;
-using ECSManager = ecs::Manager<Settings>;
+#include "ecs_settings.hpp"
 
 class Application
 {
 private:
     Window window;
     ECSManager mgr;
-    static Application* instance;
+    static Application *instance;
+
 public:
     Application();
     ~Application();
 
-    static Application& get() { return *instance; }
+    static Application &get() { return *instance; }
+    static ECSManager &get_manager() { return instance->mgr; }
 
     void run();
     void init_player();
     void init_meteor_manager();
 
-    Window& get_window() { return window; }
+    Window &get_window() { return window; }
 
-    unsigned initCircleMesh(float radius = 10.0f) {
-        std::vector<glm::vec3> vertices = std::vector<glm::vec3>(32);
+    static unsigned initCircleMesh(float radius = 10.0f)
+    {
+        static bool initialized = false;
+        static unsigned VAO;
 
-        vertices[0] = {0, 0, 0};
-
-
-        for (unsigned i = 1; i < 32; i++)
+        if (!initialized)
         {
-            float angle = (360.f/30.f) * (i - 1);
-            glm::vec3 vertice = {glm::cos(glm::radians(angle)), glm::sin(glm::radians(angle)), 0};
-            vertices[i] = vertice;
+            std::vector<glm::vec3> vertices = std::vector<glm::vec3>(32);
+
+            vertices[0] = {0, 0, 0};
+
+            for (unsigned i = 1; i < 32; i++)
+            {
+                float angle = (360.f / 30.f) * (i - 1);
+                glm::vec3 vertice = {glm::cos(glm::radians(angle)), glm::sin(glm::radians(angle)), 0};
+                vertices[i] = vertice;
+            }
+
+            std::vector<unsigned> indices = std::vector<unsigned>(96);
+
+            for (unsigned i = 0; i < 32; i++)
+            {
+                indices[0 + (i * 3)] = 0;
+                indices[1 + (i * 3)] = i + 1;
+                indices[2 + (i * 3)] = i + 2;
+            }
+
+            // indices[89] = 1;
+
+            unsigned VBO, EBO;
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glGenBuffers(1, &EBO);
+            // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+            glBindVertexArray(VAO);
+
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * indices.size(), &indices[0], GL_STATIC_DRAW);
+
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+            glEnableVertexAttribArray(0);
+
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+            initialized = true;
         }
 
-        std::vector<unsigned> indices = std::vector<unsigned>(96);
-
-        
-
-        for(unsigned i = 0; i < 32; i++) {
-            indices[0+(i*3)] = 0;
-            indices[1+(i*3)] = i+1;
-            indices[2+(i*3)] = i+2;
-        }
-
-        //indices[89] = 1;
-        
-        unsigned VBO, VAO, EBO;
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glGenBuffers(1, &EBO);
-        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-        glBindVertexArray(VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0])* vertices.size(), &vertices[0], GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * indices.size(), &indices[0], GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0); 
-        glBindVertexArray(0);
-        
         return VAO;
     }
 
-    unsigned initTriangleMesh(float radius = 10.0f) {
+    unsigned initTriangleMesh(float radius = 10.0f)
+    {
         float vertices[] = {
             -0.5f, -0.5f, 0.0f,
             0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
-        }; 
+            0.0f, 0.5f, 0.0f};
 
         unsigned indices[] = {
-            0, 1,  2,
+            0,
+            1,
+            2,
         };
-        
+
         unsigned VBO, VAO, EBO;
         glGenVertexArrays(1, &VAO);
         glGenBuffers(1, &VBO);
@@ -108,38 +102,71 @@ public:
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
         glEnableVertexAttribArray(0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
-        
+
         return VAO;
     }
 
-    unsigned initAsteroidMesh(float radius = 10.0f) {
+    unsigned initAsteroidMesh(float radius = 10.0f)
+    {
         float vertices[] = {
-            -1.f, -2.f, 0.0f,
-            -2.f, -1.f, 0.0f,
-            -2.f,  0.0f, 0.0f,
-            -1.f,  1.0f, 0.0f,
-            0.0f, 0.5f, 0.0f,
-            1.0f, 1.0f, 0.0f,
-            1.8f, 0.0f, 0.0f,
-            1.2f, -1.2f, 0.0f,
-            0.0f, -2.0f, 0.0f,
-        }; 
+            -1.f,
+            -2.f,
+            0.0f,
+            -2.f,
+            -1.f,
+            0.0f,
+            -2.f,
+            0.0f,
+            0.0f,
+            -1.f,
+            1.0f,
+            0.0f,
+            0.0f,
+            0.5f,
+            0.0f,
+            1.0f,
+            1.0f,
+            0.0f,
+            1.8f,
+            0.0f,
+            0.0f,
+            1.2f,
+            -1.2f,
+            0.0f,
+            0.0f,
+            -2.0f,
+            0.0f,
+        };
 
         unsigned indices[] = {
-            0, 1, 2,
-            0, 2, 3,
-            0, 3, 8,
-            3, 4, 8,
-            4, 7, 8,
-            4, 5, 7,
-            5, 7, 6,
+            0,
+            1,
+            2,
+            0,
+            2,
+            3,
+            0,
+            3,
+            8,
+            3,
+            4,
+            8,
+            4,
+            7,
+            8,
+            4,
+            5,
+            7,
+            5,
+            7,
+            6,
         };
-        
+
         unsigned VBO, VAO, EBO;
         glGenVertexArrays(1, &VAO);
         glGenBuffers(1, &VBO);
@@ -153,15 +180,12 @@ public:
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
         glEnableVertexAttribArray(0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
-        
+
         return VAO;
     }
-
 };
-
-
