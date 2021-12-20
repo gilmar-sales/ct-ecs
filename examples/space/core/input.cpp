c0a5f15f4cb722d6d1ed58577abc212ff2f80c2b
gilmarxd <gilmar_custodio_@hotmail.com>
2021-12-20 08:29:39 -0300

    ✨ add get key down method

--- examples/space/core/input.cpp
+++ examples/space/core/input.cpp
@@ -2,31 +2,46 @@
 
 #include "application.hpp"
 
+bool Input::GetKeyDown(KeyCode key)
+{
+    int last_state = keys[key];
+    auto *window = Application::get().get_window().get_native_window();
+    auto state = glfwGetKey(window, static_cast<int>(key));
+    bool key_down = last_state == GLFW_RELEASE && state == GLFW_PRESS;
 
-bool Input::IsKeyPressed(const KeyCode key) {
-    auto* window = Application::get().get_window().get_native_window();
+    keys[key] = state;
+    return key_down;
+}
+
+bool Input::IsKeyPressed(const KeyCode key)
+{
+    auto *window = Application::get().get_window().get_native_window();
     auto state = glfwGetKey(window, static_cast<int32_t>(key));
     return state == GLFW_PRESS || state == GLFW_REPEAT;
 }
 
-bool Input::IsMouseButtonPressed(const MouseCode button) {
-    auto* window = Application::get().get_window().get_native_window();
+bool Input::IsMouseButtonPressed(const MouseCode button)
+{
+    auto *window = Application::get().get_window().get_native_window();
     auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
     return state == GLFW_PRESS;
 }
 
-glm::vec2 Input::GetMousePosition() {
-    auto* window = Application::get().get_window().get_native_window();
+glm::vec2 Input::GetMousePosition()
+{
+    auto *window = Application::get().get_window().get_native_window();
     double xpos, ypos;
     glfwGetCursorPos(window, &xpos, &ypos);
 
-    return { (float)xpos, (float)ypos };
+    return {(float)xpos, (float)ypos};
 }
 
-float Input::GetMouseX() {
+float Input::GetMouseX()
+{
     return GetMousePosition().x;
 }
 
-float Input::GetMouseY() {
+float Input::GetMouseY()
+{
     return GetMousePosition().y;
 }
--- examples/space/core/input.hpp
+++ examples/space/core/input.hpp
@@ -5,12 +5,16 @@
 #include "keycodes.hpp"
 #include "mousecodes.hpp"
 
-class Input {
-	public:
-		static bool IsKeyPressed(KeyCode key);
+static KeyCode keys[GLFW_KEY_LAST];
 
-		static bool IsMouseButtonPressed(MouseCode button);
-		static glm::vec2 GetMousePosition();
-		static float GetMouseX();
-		static float GetMouseY();
+class Input
+{
+public:
+	static bool GetKeyDown(KeyCode key);
+	static bool IsKeyPressed(KeyCode key);
+
+	static bool IsMouseButtonPressed(MouseCode button);
+	static glm::vec2 GetMousePosition();
+	static float GetMouseX();
+	static float GetMouseY();
 };
